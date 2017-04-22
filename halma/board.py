6f7d2a60903c97884f55a1d7d19b753ad088f3dd
Kelvin Rodriguez <kelvinrr@icloud.com>
2017-04-22 16:15:46 -0700

    board __init__ function created to create start condition

--- halma/board.py
+++ halma/board.py
@@ -1,5 +1,24 @@
 # Edit this or whatever
 
-class Baord(object):
-    def __init__(self):
-        self.test = None
+class Board(object):
+
+    def __init__(self, w, h):
+        self.state = [[False for x in range(w)] for y in range(h)]
+
+        if w != h:
+            raise Exception("Width and height must be equal, got: ", w, h)
+
+        pieces = 5 if w == 5 else 6
+
+        self.width = w
+        self.height = h
+
+        for i in range(-pieces+1,0):
+            row_len = i + pieces
+            for j in range(row_len):
+                self.state[i][j] = 'r'
+
+        for i in range(-pieces+1,0):
+            row_len = i + pieces
+            for j in range(row_len):
+                self.state[j][i] = 'g'
