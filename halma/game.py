3cff84d7298217cc08cc72309245a5c0cc8a08b8
Kelvin Rodriguez <kelvinrr@icloud.com>
2017-04-22 20:27:23 -0700

    updated game object

--- halma/game.py
+++ halma/game.py
@@ -1,28 +1,28 @@
 from tkinter import *
 
-from halma.gui import HalmaGui
+from halma.gui import HalmaGUI
 from halma.board import Board
 
-def Halma(object):
+class Halma(object):
     def __init__(self, size):
         self.board = Board(size)
-        self.gui = HalmaGUI(Tk(), board)
-        self.gui.set_board(board.state)
+        self.gui = HalmaGUI(Tk(), self.board)
+        self.gui.set_board(self.board)
         self.winner = None
+        self.current_turn = random.choice(['R', 'G'])
         root.mainloop()
 
     def run_command(cmd):
         """
         commands must be in the format "b6->c7"
         """
-        def xyToCoord(x,y):
-            return chr(y + 97) + str(self.size - x)
-
-        def coordToXY(coord):
-            coord = coord.strip().lower()
-            return (ord(coord[0])-97, int(coord[1]))
-
         src, dst = cmd.split("->")
-        src = coordToXY(src)
-        dst = coordToXY(dst)
+        src = self.board.coordToXY(src)
+        dst = self.board.coordToXY(dst)
         self.board.move(src, dst)
+
+
+
+if __name__ == "__main__":
+    game = Halma(10)
+    print(game.current_turn)
