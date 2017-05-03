fde8d4125afdb93960b0eb541f918bc0f2a3312c
Kelvin Rodriguez <kelvinrr@icloud.com>
2017-05-02 20:37:45 -0700

    Thatcher is a polynigmion

--- halma/tree.py
+++ halma/tree.py
@@ -1,12 +1,5 @@
-class Tree(object):
-    def __init__(self, children=None, score=0, state=None):
-        self.score = score
-        self.state = state
-        self.children = children
 
-    def __repr__(self):
-        return self.name
+import collections
+from collections import namedtuple
 
-    def add_child(self, node):
-        assert isinstance(node, Tree)
-        self.children.add(node)
+Tree = namedtuple('Tree', ['state', 'score', 'children'])
