c55a9d93e4def1162d7a405d1d20c717b84f6fa7
Kelvin Rodriguez <kelvinrr@icloud.com>
2017-05-02 23:46:18 -0700

    things and stuff

--- halma/ai.py
+++ halma/ai.py
@@ -2,10 +2,9 @@ from halma.board import Board
 from halma.tree import Tree
 
 class AI(object):
-    
     def __init__(self, board):
         self.board = board
-    
+
     def gen_tree(self, team, opp, depth, player, move):
         children = []
         if player:
@@ -16,14 +15,14 @@ class AI(object):
             teams = (team, opp)
             minimax = max
             score = 0
-        
+
         if not depth == 0:
             for src in team:
                 for dest in self.board.get_valid_moves(src):
                     teamC = team.copy()
                     self.board.sub_move(dest,src, teamC)
                     child = self.gen_tree(opp, teamC, depth - 1, not player, (src, dest))
-                    score = minimax(score, child.score) 
+                    score = minimax(score, child.score)
                     children.append(child)
-        
+
         return Tree(teams, score, children, move)
