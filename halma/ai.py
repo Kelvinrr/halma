7c8bf4e6fc6a2e9b40162b252b1fe9a293610d3b
Jack <jg2562@nau.edu>
2017-05-02 23:13:55 -0700

    Fixed ai to working status

--- halma/ai.py
+++ halma/ai.py
@@ -7,7 +7,7 @@ class AI(object):
         self.board = board
     
     def gen_tree(self, team, opp, depth, player, move):
-        children= set()
+        children = []
         if player:
             teams = (opp, team)
             minimax = min
@@ -17,18 +17,13 @@ class AI(object):
             minimax = max
             score = 0
         
-        sub_tree = Tree(teams, 0, children, move)
-        if depth == 0:
-            sub_tree.score = 0;
-            return tree
-
-        for src in team:
-            for dest in board.get_valid_moves(src):
-                teamC = team.copy()
-                self.board.sub_move(dest,src, teamC)
-                child = gen_tree(tree, opp, teamC, depth - 1, not player, (src, dest))
-                
-                score = minimax(score, child) 
-                children.add(child)
-        sub_tree.score = score
-        return sub_tree
+        if not depth == 0:
+            for src in team:
+                for dest in self.board.get_valid_moves(src):
+                    teamC = team.copy()
+                    self.board.sub_move(dest,src, teamC)
+                    child = self.gen_tree(opp, teamC, depth - 1, not player, (src, dest))
+                    score = minimax(score, child.score) 
+                    children.append(child)
+        
+        return Tree(teams, score, children, move)
