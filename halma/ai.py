8c2f2e78974e1a4513c25468ecf250aefc68035d
Jack <jg2562@nau.edu>
2017-05-02 22:03:49 -0700

    Actually added ai to game

--- halma/ai.py
+++ halma/ai.py
@@ -1,7 +1,30 @@
+import board
+from tree import Tree
+
 class AI(object):
-    def __init__():
-        pass
+    def __init__(board):
+        self.board = board
     
-    def gen_tree(depth):
+    def gen_tree(team, opp, depth, player, move):
+        children= set()
+        if player:
+            teams = (opp, team)
+            minimax = min
+            score = float('inf')
+        else:
+            teams = (team, opp)
+            minimax = max
+            score = 0
         
-        
+        sub_tree = Tree(teams, 0, children, move)
+        if depth == 0:
+            sub_tree.score = 0;
+            return tree
+
+        for src in team:
+            for dest in board.get_valid_moves(src):
+                child = gen_tree(tree, opp, team, depth - 1, not player, (src, dest))
+                score = minimax(score, child) 
+                children.add(child)
+        sub_tree.score = score
+        return sub_tree
